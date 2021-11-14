2395e418bef378b7de80857ee0fb7f53e3190b90
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2021-11-14 23:34:40 +0800

    Add prototype

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def gen_big5_hex():
     # BIG5 為雙位元組字元集
-    high_bytes_from = 0x81 << 8
-    high_bytes_to = 0xFE << 8
+    high_bytes_from = 0x81
+    high_bytes_to = 0xFE
 
     low_bytes_from = 0x40
     low_bytes_to = 0xFF
-    pass
+    for high in range(high_bytes_from, high_bytes_to):
+        for low in range(low_bytes_from, low_bytes_to):
+            # print(f'{hex(high << 8)} + {hex(low)} = {hex((high << 8) + low)} = {bytes.fromhex(hex((high << 8) + low)).decode("big5")}')
+            yield hex((high << 8) + low)
 
 
 def gen_big5_table(codecs_name):
@@ -28,4 +31,10 @@ def gen_big5_table(codecs_name):
 
 
 if __name__ == '__main__':
-    gen_big5_table('cp950')
+    # gen_big5_table('cp950')
+    for i in gen_big5_hex():
+        try:
+            print(bytes.fromhex(i[2:]).decode("big5"))
+        except:
+            pass
+            # print('None')
