f4c4e355cf2032ca0c88996b26a458731dfbbacb
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2021-11-16 23:19:05 +0800

    Generate codecs table

--- main.py
+++ main.py
@@ -1,40 +1,75 @@
-def gen_big5_hex():
+def gen_big5_hex() -> tuple[hex, hex]:
+    """
+    產出 Big5 內碼 Table
+    :return: 回傳 Tuple[ 現行 Hex , 低位 Hex ]
+    """
     # BIG5 為雙位元組字元集
+    # 高位元數列為: 0x81-0xFE
     high_bytes_from = 0x81
     high_bytes_to = 0xFE
 
+    # 低位元數列為: 0x40-0x7E 、 及0xA1-0xFE 兩組
     low_bytes_from = 0x40
     low_bytes_to = 0xFF
+    # 排除掉以下 (位於 0x7E 至 0xA1 數列)
+    low_bytes_exclude_from = 0x7F
+    low_bytes_exclude_to = 0xA0
     for high in range(high_bytes_from, high_bytes_to):
         for low in range(low_bytes_from, low_bytes_to):
-            # print(f'{hex(high << 8)} + {hex(low)} = {hex((high << 8) + low)} = {bytes.fromhex(hex((high << 8) + low)).decode("big5")}')
-            yield hex((high << 8) + low)
+            if low not in range(low_bytes_exclude_from, low_bytes_exclude_to + 1):
+                yield hex((high << 8) + low), hex(low)
 
 
 def gen_big5_table(codecs_name):
-    last_char_list = [i for i in range(15)]
-    # last_char_list.extend(['A', 'B', 'C', 'D', 'E', 'F'])
-    # for i in last_char_list:
-    #     print(i)
+    """
+    依照編碼, 產出一張內碼Table
+    :param codecs_name: Big5 類
+    :return: Print 出 Table
+    """
+    # Print Header
+    print('　　｜　+1　+2　+3　+4　+5　+6　+7　+8　+9　+A　+B　+c　+D　+E　+F')
 
-    from_code = 0xA440
-    to_code = 0xC67E
-    for code in range(from_code, to_code):
-        for i in last_char_list:
-            if code + i > to_code:
-                break
-            try:
-                # print(f'code: {code}, int: {code + i}, words: {bytes.fromhex(hex(code + i)[2:]).decode("big5")}')
-                bytes.fromhex(hex(code + i)[2:]).decode(codecs_name)
-            except:
-                print(hex(code + i))
+    current_hex_head = ''
+    msg = ''
+    for big5_hex, low_hex in gen_big5_hex():
+        hex_str = big5_hex[2:]
+        low_hex_str = low_hex[2:]
+        hex_head = hex_str[:3]
+        if current_hex_head == hex_head:
+            char = get_decoded_char(codecs_name, hex_str)
+            if char is not None:
+                msg += f'　{char}'
+            else:
+                msg += f'　None'
+
+            # 若低位元為 '7e' or 'fe', 則下一個肯定不存在 (ff, 7f), 原因為低位元只到 0x7E 以及 0xFE
+            if low_hex_str.lower() in ['7e', 'fe']:
+                msg += f'　Undefined'
+        else:
+            # 印出累積的資料行
+            print(msg)
+            msg = ''
+
+            current_hex_head = hex_head
+            msg += f'{current_hex_head}　｜　'
+
+            # 若低位元為 'a1', 則上一個肯定不存在 (a0), 原因為 0xA0 位於低位排除名單內
+            if low_hex_str.lower() == 'a1':
+                msg += f'　Undefined'
+            char = get_decoded_char(codecs_name, hex_str)
+            if char is not None:
+                msg += f'　{char}'
+            else:
+                msg += f'　None'
+
+
+def get_decoded_char(codecs_name, hex_str):
+    try:
+        char = bytes.fromhex(hex_str).decode(codecs_name)
+        return char
+    except UnicodeDecodeError as e:
+        return None
 
 
 if __name__ == '__main__':
-    # gen_big5_table('cp950')
-    for i in gen_big5_hex():
-        try:
-            print(bytes.fromhex(i[2:]).decode("big5"))
-        except:
-            pass
-            # print('None')
+    gen_big5_table('cp950')
